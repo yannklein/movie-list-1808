30f68809a247268d2c58d1ce46af804abc37cbfe
Yann Klein <yann.klein@me.com>
2024-05-01 10:41:47 +0900

    lecture done

--- javascript/application.js
+++ javascript/application.js
@@ -1,36 +1,34 @@
 import Mustache from "mustachejs";
 import { createApp } from "vue";
 
-console.log("Good Wed!");
+const url = `https://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7`;
 
 // ///////////
 // Vanilla JS
 // ///////////
 
-// // 1. Select elements (results)
-// const results = document.getElementById("results");
+// // 1. Select elements results div
+// const results = document.querySelector('#results');
 
-// // 2. Fetch ombdbAPI
-// const url = "https://www.omdbapi.com/?s=matrix&apikey=adf1f2d7"
+// // 2. Fetch an array movies form OMDB API
 // fetch(url)
-//   .then(response => response.json())
+//   .then((response) => response.json())
 //   .then((data) => {
-//     console.log(data.Search);
+//     // 3. Change the DOM insert the movies in results
 //     const movies = data.Search;
-//     // 3. Change the DOM, insert the movies in the result
 //     movies.forEach((movie) => {
-//       results.insertAdjacentHTML("afterbegin", `
+//       const cardTemplate = `
 //       <div class='col-6'>
 //         <div class="card">
-//           <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title}">
+//           <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title} poster">
 //           <div class="card-body">
-//             <h6 class="card-title">${movie.Title}</h6>
+//             <h2 class="card-title">${movie.Title}</h2>
 //             <p class="card-text">${movie.Year}</p>
 //             <a href="https://www.imdb.com/title/${movie.imdbID}" class="btn btn-primary" target="_blank">Go on IMDB</a>
 //           </div>
 //         </div>
-//       </div>
-//       `);
+//       </div>`;
+//       results.insertAdjacentHTML("beforeend", cardTemplate);
 //     });
 //   });
 
@@ -38,98 +36,74 @@ console.log("Good Wed!");
 // JS with templates
 // ///////////
 
-// // // 1. Select elements (results)
-// const results = document.getElementById("results");
-// const template = document.getElementById("movieCardTemplate");
-// // console.log(template);
-// // // 2. Fetch ombdbAPI
-// const url = "https://www.omdbapi.com/?s=matrix&apikey=adf1f2d7"
+// const template = document.querySelector("#movie-card-template");
+// const results = document.querySelector('#results');
+
 // fetch(url)
-//   .then(response => response.json())
+//   .then((response) => response.json())
 //   .then((data) => {
 //     const movies = data.Search;
-//     // 3. Change the DOM add cardinfo into the results
 //     movies.forEach((movie) => {
-//       const clone = template.content.cloneNode(true);
-//       clone.querySelector("h6.card-title").innerText = movie.Title;
-//       clone.querySelector("p.card-text").innerText = movie.Year;
-//       clone.querySelector("img.card-img-top").src = movie.Poster;
-//       clone.querySelector("img.card-img-top").alt = movie.Title;
-//       clone.querySelector("a.btn-primary").href += movie.imdbID;
-//       console.log(clone);
-//       results.appendChild(clone);
+//       console.log(template.content) // this is a HTML element
+//       // console.log(template.innerHTML) // this is a string
+//       const clone = template.content.cloneNode(true); // creating a copy of the original template
+//       clone.querySelector(".card-title").innerText = movie.Title;
+//       clone.querySelector(".card-img-top").src = movie.Poster;
+//       clone.querySelector(".card-img-top").alt = `${movie.Title} poster`;
+//       clone.querySelector("a").href = `https://www.imdb.com/title/${movie.imdbID}`;
+//       results.appendChild(clone) // inserting the clone into results
 //     });
 //   });
-    
-    
-// "h6.card-title"
-// <h6 class="card-title">Matches this!</h6>
-
-// h6 .card-title
-// <h6>
-//   <div class="card-title">Matches this!</div>
-// </h6>
-
 
 // ///////////
 // MUSTACHEJS
 // ///////////
 
-// const testTemplate = "<h4>{{subTitle}}</h4>";
-// const testData = {
-//   subTitle: "Yoshio is the bomb"
-// };
-// const rendered = Mustache.render(testTemplate, testData);
-// // <h4>{{subTitle}}</h4>
-// // <h4>Yoshio is the bomb</h4>
-// console.log(rendered);
+// const testTemplate = "<h1>{{student}} is amazing!</h1>";
+// const testData = { student: "Rei" }
 
-// // Movie data structure:
-// // {
-// //   "Title": "Harry Potter and the Deathly Hallows: Part 2",
-// //   "Year": "2011",
-// //   "imdbID": "tt1201607",
-// //   "Type": "movie",
-// //   "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-// // }
+// const testResult = Mustache.render(testTemplate, testData);
+// console.log(testResult);
+// => <h1>Rei is amazing!</h1>
 
-// // // // 1. Select elements (results)
-// const results = document.getElementById("results");
-// const template = document.getElementById("movieCardTemplate");
+// const template = document.querySelector("#movie-card-template");
+// const results = document.querySelector('#results');
 
-// // 2. Fetch ombdbAPI
-// const url = "https://www.omdbapi.com/?s=matrix&apikey=adf1f2d7"
 // fetch(url)
-//   .then(response => response.json())
+//   .then((response) => response.json())
 //   .then((data) => {
 //     const movies = data.Search;
-//     // 3. Change the DOM add cardinfo into the results
 //     movies.forEach((movie) => {
-//       const output = Mustache.render(template.innerHTML, movie);
-//       results.insertAdjacentHTML("beforeend", output);
+//       // {
+//       //   "Title": "Harry Potter and the Goblet of Fire",
+//       //   "Year": "2005",
+//       //   "imdbID": "tt0330373",
+//       //   "Type": "movie",
+//       //   "Poster": "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_SX300.jpg"
+//       //  }
+//       const cardTemplate = template.innerHTML;
+//       const card = Mustache.render(cardTemplate, movie);
+//       results.insertAdjacentHTML("beforeend", card);
 //     });
 //   });
 
-
-  // ///////////
+// ///////////
 // MUSTACHEJS + ITERATION
 // ///////////
 
-// // // // 1. Select elements (results)
-// const results = document.getElementById("results");
-// const template = document.getElementById("movieCardTemplate");
+// const template = document.querySelector("#movie-card-template");
+// const results = document.querySelector('#results');
 
-// // 2. Fetch ombdbAPI
-// const url = "https://www.omdbapi.com/?s=matrix&apikey=adf1f2d7"
 // fetch(url)
-//   .then(response => response.json())
+//   .then((response) => response.json())
 //   .then((data) => {
-//     // 3. Change the DOM add cardinfo into the results
+//     const movies = data.Search;
 //     const moviesData = {
-//       movies: data.Search
+//       "movies": movies
 //     }
-//     const output = Mustache.render(template.innerHTML, moviesData);
-//     results.insertAdjacentHTML("beforeend", output);
+//     const cardTemplate = template.innerHTML;
+//     const cards = Mustache.render(cardTemplate, moviesData);
+//     results.innerHTML = cards;
 //   });
 
 // ///////////
@@ -139,7 +113,7 @@ console.log("Good Wed!");
 createApp({
   data() {
     return {
-      message: "Hello Yoshio!"
+      message: "Hello batch #1638"
     }
   }
-}).mount("#vue-root");
+}).mount("#vue-root")
